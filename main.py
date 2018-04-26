899e15689474b4198df3ca99cdb7fe75548b8e4d
liuzongyao <13851618795@126.com>
2018-04-26 18:07:22 +0800

    update main

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
 
     pytest.main(run_command)
 
-    # var = 1
-    # while var == 1:  # 该条件永远为true，循环将无限执行下
-    #     print "hello world"
+    var = 1
+    while var == 1:  # 该条件永远为true，循环将无限执行下
+         print "hello world"
