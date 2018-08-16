74b45398467b74634ddd2415a1024b548ac574f5
alauda <354068083@qq.com>
2018-08-16 14:43:48 +0800

    1、镜像同步模块的e2e，目前只覆盖了从私有到公有的镜像同步 2、镜像仓库的e2e，目前只覆盖了创建、删除镜像仓库项目以及创建、更新、删除镜像仓库

--- common/match_case.py
+++ common/match_case.py
@@ -2,6 +2,7 @@
 def casename():
     casename_dict = {
         "test_newk8s_app": "应用测试",
-        "test_svn_build": "svn快速构建"
+        "test_svn_build": "svn快速构建",
+        "test_sync_public_registry": "镜像同步"
     }
     return casename_dict
