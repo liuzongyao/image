b742655958e90bcb3cb6e805f266e2a21fe6f393
alauda <354068083@qq.com>
2018-08-16 15:39:25 +0800

    Merge branch 'master' of https://bitbucket.org/mathildetech/alauda-api-automation
    # Conflicts:
    #	common/match_case.py

--- test_case/image/test_images.py
+++ test_case/image/test_images.py
@@ -8,9 +8,9 @@ class TestImageSuite(object):
     def setup_class(self):
         self.image_tool = Image()
 
-        self.reg_project_name = 'e2e-reg-project-{}'.format(self.image_tool.registry_name)
+        self.reg_project_name = 'alauda-reg-project-{}'.format(self.image_tool.registry_name)
         self.update_repo = "update daemon"
-        self.repo_name = 'e2e-repo-{}'.format(self.image_tool.registry_name)
+        self.repo_name = 'alauda-repo-{}'.format(self.image_tool.registry_name)
 
         self.image_tool.delete_repo(self.repo_name, reg_project_name=self.reg_project_name)
         self.image_tool.delete_reg_project(self.reg_project_name)
@@ -56,6 +56,15 @@ class TestImageSuite(object):
         delete_repo_ret = self.image_tool.delete_repo(self.repo_name, reg_project_name=self.reg_project_name)
         assert delete_repo_ret.status_code == 204, delete_repo_ret.text
 
+        # verify delete result
+        get_repo_ret = self.image_tool.get_repo_detail(self.repo_name, reg_project_name=self.reg_project_name)
+        assert get_repo_ret.status_code == 404, "delete repo failed, Error code:{}, Response: {}".format(
+            get_repo_ret.status_code, get_repo_ret.text)
+
         # delete registry project
         delete_reg_ret = self.image_tool.delete_reg_project(self.reg_project_name)
         assert delete_reg_ret.status_code == 204, delete_reg_ret.text
+
+        # verify delete result
+        get_reg_project_ret = self.image_tool.get_reg_project(self.reg_project_name)
+        assert get_reg_project_ret is False, "delete registry project failed"
