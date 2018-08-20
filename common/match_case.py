d2f5d005bf28af0e95fe864f34bbe6bca599d02d
shijingnan <jnshi@alauda.io>
2018-08-20 19:18:52 +0800

    test scs

--- common/match_case.py
+++ common/match_case.py
@@ -5,6 +5,8 @@ def casename():
         "test_dashboard": "监控面板相关测试",
         "test_svn_build": "svn快速构建",
         "test_sync_public_registry": "镜像同步",
+        "test_ci_cd": "持续集成",
+        "test_noti": "通知增删改查测试",
         "test_gfs_app": "应用使用gfs测试",
         "test_ebs_app": "应用使用ebs测试",
         "test_pvc_app": "应用使用pvc测试",
@@ -14,6 +16,9 @@ def casename():
         "test_ci_cd": "持续集成",
         "test_noti": "通知增删改查测试",
         "test_pvc": "持久卷声明测试",
-        "test_job": "任务测试"
+        "test_job": "任务测试",
+        "test_pvc_use_scs": "持久卷声明使用存储类测试",
+        "test_pvc_use_defaultscs": "持久卷声明使用默认存储类测试",
+        "test_scs": "存储类测试",
     }
     return casename_dict
