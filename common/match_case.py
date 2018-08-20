98010cbd88124e90d3b346170be5e9214ed0b444
shijingnan <jnshi@alauda.io>
2018-08-20 19:18:52 +0800

    test scs

--- common/match_case.py
+++ common/match_case.py
@@ -5,14 +5,17 @@ def casename():
         "test_dashboard": "监控面板相关测试",
         "test_svn_build": "svn快速构建",
         "test_sync_public_registry": "镜像同步",
+        "test_ci_cd": "持续集成",
+        "test_noti": "通知增删改查测试",
         "test_gfs_app": "应用使用gfs测试",
         "test_ebs_app": "应用使用ebs测试",
         "test_pvc_app": "应用使用pvc测试",
         "test_gfs_volume": "存储卷gfs测试",
         "test_ebs_volume": "存储卷ebs测试",
         "test_pv": "持久卷测试",
-        "test_ci_cd": "持续集成",
-        "test_noti": "通知增删改查测试",
-        "test_pvc": "持久卷声明测试"
+        "test_pvc": "持久卷声明测试",
+        "test_pvc_use_scs": "持久卷声明使用存储类测试",
+        "test_pvc_use_defaultscs": "持久卷声明使用默认存储类测试",
+        "test_scs": "存储类测试",
     }
     return casename_dict
--- test_case/persistentvolumeclaims/test_pvc.py
+++ test_case/persistentvolumeclaims/test_pvc.py
@@ -1,7 +1,10 @@
+import time
+
 import pytest
 
 from test_case.persistentvolumeclaims.pvc import Pvc
 from test_case.persistentvolumes.pv import Pv
+from test_case.storageclasses.scs import Scs
 from test_case.volume.volume import Volume
 
 
@@ -15,9 +18,23 @@ class TestPvSuite(object):
         self.pv_name = 'alauda-pv-{}'.format(self.pv.region_name).replace('_', '-')
         self.pvc = Pvc()
         self.pvc_name = 'alauda-pvc-{}'.format(self.pvc.region_name).replace('_', '-')
+
+        self.scs = Scs()
+        self.scs_name = 'alauda-scsforpvc-{}'.format(self.pvc.region_name).replace('_', '-')
+        self.pvcusescs_name = 'alauda-pvcusescs-{}'.format(self.pvc.region_name).replace('_', '-')
+        self.masterips = self.scs.global_info["$MASTERIPS"].split(",")
+        self.default_size = self.scs.get_default_size()
+
+        self.defaultscs_name = 'alauda-defaultscsforpvc-{}'.format(self.pvc.region_name).replace('_', '-')
+        self.pvcusedefaultscs_name = 'alauda-pvcusedefaultscs-{}'.format(self.pvc.region_name).replace('_', '-')
+
         self.teardown_class(self)
 
     def teardown_class(self):
+        self.scs.delete_scs(self.defaultscs_name)
+        self.pvc.delete_pvc(self.pvc.global_info["$K8S_NAMESPACE"], self.pvcusedefaultscs_name)
+        self.scs.delete_scs(self.scs_name)
+        self.pvc.delete_pvc(self.pvc.global_info["$K8S_NAMESPACE"], self.pvcusescs_name)
         self.pvc.delete_pvc(self.pvc.global_info["$K8S_NAMESPACE"], self.pvc_name)
         self.pv.delete_pv(self.pv_name)
         volume_id = self.volume.get_volume_id_from_list(self.volume_name)
@@ -68,4 +85,69 @@ class TestPvSuite(object):
         assert delete_result.status_code == 204
         assert self.pvc.check_exists(self.pvc.get_common_pvc_url(self.pvc.global_info["$K8S_NAMESPACE"], self.pvc_name),
                                      404)
+        assert result['flag'], result
+
+    @pytest.mark.scs
+    def test_pvc_use_scs(self):
+        result = {"flag": True}
+        # create scs
+        if len(self.masterips) == 0:
+            assert True, "获取master节点失败，不能创建正常使用的存储类"
+            return
+        masterip = self.masterips[0]
+        create_result = self.scs.create_scs("./test_data/scs/scs.yml",
+                                            {"$scs_name": self.scs_name, "$is_default": "false",
+                                             "$master_ip": masterip})
+        assert create_result.status_code == 201, create_result.text
+
+        # create pvc
+        createpvc_result = self.pvc.create_pvc("./test_data/pvc/pvc.json",
+                                               {"$pvc_name": self.pvcusescs_name, "$pvc_mode": "ReadWriteOnce",
+                                                "$scs_name": self.scs_name, "$size": "1"})
+        assert createpvc_result.status_code == 201, createpvc_result.text
+        time.sleep(60)
+        # get pvc detail
+        detail_result = self.pvc.get_pvc_detail(self.pvc.global_info["$K8S_NAMESPACE"], self.pvcusescs_name)
+        result = self.pvc.update_result(result, detail_result.status_code == 200, detail_result.text)
+        result = self.pvc.update_result(result, self.pvc.get_value(detail_result.json(), "status.phase") == "Bound",
+                                        detail_result.text)
+        result = self.pvc.update_result(result, self.pvc.get_value(detail_result.json(),
+                                                                   "kubernetes.spec.volumeName").startswith("pvc"),
+                                        detail_result.text)
+        self.pvc.delete_pvc(self.pvc.global_info["$K8S_NAMESPACE"], self.pvcusescs_name)
+        self.scs.delete_scs(self.scs_name)
+        assert result['flag'], result
+
+    @pytest.mark.scs
+    def test_pvc_use_defaultscs(self):
+        if self.default_size > 1:
+            assert True, "有两个以上的默认存储类，无法测试"
+            return
+        elif self.default_size == 0:
+            # create scs
+            if len(self.masterips) == 0:
+                assert True, "获取master节点失败，不能创建正常使用的存储类"
+                return
+            masterip = self.masterips[0]
+            create_result = self.scs.create_scs("./test_data/scs/scs.yml",
+                                                {"$scs_name": self.defaultscs_name, "$is_default": "true",
+                                                 "$master_ip": masterip})
+            assert create_result.status_code == 201, create_result.text
+        result = {"flag": True}
+        # create pvc
+        createpvc_result = self.pvc.create_pvc("./test_data/pvc/pvc_usedefault.json",
+                                               {"$pvc_name": self.pvcusedefaultscs_name, "$pvc_mode": "ReadWriteOnce",
+                                                "$size": "1"})
+        assert createpvc_result.status_code == 201, createpvc_result.text
+        time.sleep(60)
+        # get pvc detail
+        detail_result = self.pvc.get_pvc_detail(self.pvc.global_info["$K8S_NAMESPACE"], self.pvcusedefaultscs_name)
+        result = self.pvc.update_result(result, detail_result.status_code == 200, detail_result.text)
+        result = self.pvc.update_result(result, self.pvc.get_value(detail_result.json(), "status.phase") == "Bound",
+                                        detail_result.text)
+        result = self.pvc.update_result(result, self.pvc.get_value(detail_result.json(),
+                                                                   "kubernetes.spec.volumeName").startswith("pvc"),
+                                        detail_result.text)
+        self.pvc.delete_pvc(self.pvc.global_info["$K8S_NAMESPACE"], self.pvcusedefaultscs_name)
+        self.scs.delete_scs(self.defaultscs_name)
         assert result['flag'], result
