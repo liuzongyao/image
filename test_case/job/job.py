d9055f6befcf3989510ca99533d6430a3a4116f4
brzhao <brzhao@alauda.io>
2018-08-21 20:13:35 +0800

    fix/change something

--- test_case/job/job.py
+++ test_case/job/job.py
@@ -1,4 +1,5 @@
 from common.base_request import Common
+from common.log import logger
 
 
 class Job(Common):
@@ -10,10 +11,10 @@ class Job(Common):
         return job_id and "v1/jobs/{}/{}".format(self.account,job_id) or \
                "v1/jobs/{}".format(self.account)
 
-    def create_job_config(self,file,data):
+    def create_job_config(self, file, data):
         url = self.get_job_config_url()
         data = self.generate_data(file, data)
-        return self.send(method="POST", path=url,data=data)
+        return self.send(method="POST", path=url, data=data)
 
     def get_job_config(self, config_id):
         url = self.get_job_config_url(config_id)
@@ -27,37 +28,39 @@ class Job(Common):
     def delete_job_config(self, job_name):
         config_id = self.get_config_id(job_name)
         url = self.get_job_config_url(config_id)
-        return self.send(method="DETELE", path=url)
+        return self.send(method="DELETE", path=url)
 
     def trigger_job_config(self,config_id):
+        logger.info("************************** trigger job ********************************")
         url = self.get_job_url()
-        data = {"job_config_name":config_id}
+        data = {"config_name":config_id}
         params = {"namespaces": self.account}
         return self.send(method="POST", path=url, json=data,params=params)
 
     def get_job_status(self,job_id):
         url = self.get_job_url(job_id=job_id)
-        return self.get_status(url, 'status' , 'S')
+        return self.get_status(url, 'status', 'SUCCEEDED')
 
-    def get_job_logs(self,job_id):
+    def get_job_log(self,job_id):
+        logger.info("************************** get job log ********************************")
         url = self.get_job_url(job_id=job_id) + '/logs'
-        return self.get_logs(url,"Removing the temp dir")
+        return self.get_logs(url,"hello")
 
     def delete_job(self,job_name):
         job_id = self.get_job_id(job_name)
         url = self.get_job_url(job_id)
-        return self.send(method="DETELE", path=url)
+        return self.send(method="DELETE", path=url)
 
     def get_job_id(self,job_name):
         url = self.get_job_url()
         params = {"page": 1, "page_size":20}
         response = self.send(method="GET", path=url, params=params)
         assert response.status_code == 200, "get job list fail"
-        return self.get_uuid_accord_name(response.json().get("result"),{"name":job_name}, "config_uuid")
+        return self.get_uuid_accord_name(response.json().get("results"),{"name":job_name}, "config_uuid")
 
     def get_config_id(self, job_name):
         url = self.get_job_config_url()
         params = {"page": 1, "page_size":20}
         response = self.send(method="GET", path=url, params=params)
         assert response.status_code == 200, "get job_config list failed"
-        return self.get_uuid_accord_name(response.json().get("result"),{"config_name":job_name}, "config_uuid")
+        return self.get_uuid_accord_name(response.json().get("results"),{"config_name":job_name}, "config_uuid")
--- test_case/job/test_job.py
+++ test_case/job/test_job.py
@@ -7,7 +7,7 @@ from test_case.job.job import Job
 class TestJobSuit(object):
     def setup_class(self):
         self.job = Job()
-        self.job_config_name = "e2e-job-configs-{}".format(self.job.region_name)
+        self.job_config_name = "e2e-job{}".format(self.job.region_name)
 
     def teardown_class(self):
         self.job.delete_job_config(self.job_config_name)
@@ -15,24 +15,21 @@ class TestJobSuit(object):
     def test_job_config(self):
         result = {"flag": True}
         self.job.delete_job_config(self.job_config_name)
-
-        # create job_configs
-        ret_create = self.job.create_job_config("./test_data/job/job_config.json", {"$NAME": self.job_config_name})
+        ret_create = self.job.create_job_config("./test_data/job/job_config.json", {"$CONFIG_NAME": self.job_config_name})
         assert ret_create.status_code == 201, ret_create.text
-        config_id = ret_create.json(["config_uuid"])
+        config_id = ret_create.json()["config_uuid"]
 
-        # get job_event
-
-        ret_trigger = self.trigger_job_config(config_id)
+        ret_trigger = self.job.trigger_job_config(config_id)
         assert ret_trigger.status_code == 201, ret_trigger.text
 
-        job_id = ret_trigger.json()["job_id"]
-        ret_status = self.get_job_status(job_id)
+        job_id = ret_trigger.json()["job_uuid"]
+        ret_status = self.job.get_job_status(job_id)
         assert ret_status, "job failed"
 
-        ret_log = self.get_job_logs(job_id)
-        result = self.update_result(result, ret_log, "get build log")
+        ret_log = self.job.get_job_log(job_id)
+        result = self.job.update_result(result, ret_log, "get job log")
 
-        ret_del = self.delete_job(self.job_name)
+        # delete job
+        ret_del = self.job.delete_job_config(self.job_config_name)
         assert ret_del.status_code == 204, ret_del.text
         assert result["flag"], result
