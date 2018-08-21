6b293b0890d737fc66b6c7f882e12a2d63ab7317
houchao <hchan@alauda.io>
2018-08-21 11:38:43 +0800

    add dashboard case

--- common/match_case.py
+++ common/match_case.py
@@ -2,6 +2,7 @@
 def casename():
     casename_dict = {
         "test_newk8s_app": "应用测试",
+        "test_dashboard": "监控面板相关测试",
         "test_svn_build": "svn快速构建",
         "test_sync_public_registry": "镜像同步",
         "test_gfs_app": "应用使用gfs测试",
