ef37c208a30444aefee5058f1bdce6cd45b6d3e3
brzhao <brzhao@alauda.io>
2018-08-22 12:47:26 +0800

    fix:josn data writed died

--- test_case/job/test_job.py
+++ test_case/job/test_job.py
@@ -8,6 +8,9 @@ class TestJobSuit(object):
     def setup_class(self):
         self.job = Job()
         self.job_config_name = "e2e-job{}".format(self.job.region_name)
+        self.job_registry_index = self.job.global_info["$IMAGE"].split("/")[0]
+        self.job_image_name = self.job.global_info["$IMAGE"].split("/",1)[1].split(':')[0]
+        self.job_image_tag = self.job.global_info["$IMAGE"].split(":")[1]
 
     def teardown_class(self):
         self.job.delete_job_config(self.job_config_name)
@@ -15,7 +18,12 @@ class TestJobSuit(object):
     def test_job_config(self):
         result = {"flag": True}
         self.job.delete_job_config(self.job_config_name)
-        ret_create = self.job.create_job_config("./test_data/job/job_config.json", {"$CONFIG_NAME": self.job_config_name})
+
+        ret_create = self.job.create_job_config("./test_data/job/job_config.json",
+                                                {"$CONFIG_NAME": self.job_config_name,
+                                                 "$REPO": self.job_registry_index,
+                                                 "$IMNAME": self.job_image_name,
+                                                 "$IMTAG": self.job_image_tag})
         assert ret_create.status_code == 201, ret_create.text
         config_id = ret_create.json()["config_uuid"]
 
@@ -24,12 +32,12 @@ class TestJobSuit(object):
 
         job_id = ret_trigger.json()["job_uuid"]
         ret_status = self.job.get_job_status(job_id)
-        assert ret_status, "job failed"
+        assert ret_status, "trigger job failed expected result is SUCCEEDED, actual result is {}".format(ret_status)
 
         ret_log = self.job.get_job_log(job_id)
-        result = self.job.update_result(result, ret_log, "get job log")
+        result = self.job.update_result(result, ret_log ,"get job log expexcted to contain hello")
 
         # delete job
         ret_del = self.job.delete_job_config(self.job_config_name)
         assert ret_del.status_code == 204, ret_del.text
-        assert result["flag"], result
+        assert result["flag"], "delete job_config result is {}".format(result)
