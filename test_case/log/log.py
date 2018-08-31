3de7759dd09755e79998e74925eabdba2c0fb1c3
shijingnan <jnshi@alauda.io>
2018-08-31 14:08:23 +0800

    test log

---
+++ test_case/log/log.py
@@ -0,0 +1,79 @@
+import sys
+
+from common.base_request import Common
+from common import settings
+from common.log import logger
+
+
+class Log(Common):
+    def get_logsource_url(self):
+        return "v1/integrations/{}/?families=LogSource&namespace={}".format(settings.ACCOUNT, settings.ACCOUNT)
+
+    def get_type_url(self):
+        return "v2/logs/{}/types?read_log_source_uuid=default&namespace={}".format(settings.ACCOUNT, settings.ACCOUNT)
+
+    def get_aggregations_url(self):
+        return "v2/logs/{}/aggregations?read_log_source_uuid=default&paths=&namespace={}".format(settings.ACCOUNT,
+                                                                                                 settings.ACCOUNT)
+
+    def get_search_url(self, service_id):
+        return "v2/logs/{}/search?services={}&read_log_source_uuid=default&paths=&namespace={}".format(settings.ACCOUNT,
+                                                                                                       service_id,
+                                                                                                       settings.ACCOUNT)
+
+    def get_saved_search_url(self, display_name=''):
+        return "v2/logs/{}/saved_search?all=true&display_name={}&namespace={}".format(settings.ACCOUNT, display_name,
+                                                                                      settings.ACCOUNT)
+
+    def get_common_saved_search_url(self, uuid=''):
+        return "v2/logs/{}/saved_search/{}?&namespace={}".format(settings.ACCOUNT, uuid, settings.ACCOUNT)
+
+    def list_saved_search(self, display_name=''):
+        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
+        url = self.get_saved_search_url(display_name)
+        return self.send(method='get', path=url)
+
+    def create_saved_search(self, file, data):
+        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
+        url = self.get_common_saved_search_url()
+        data = self.generate_data(file, data)
+        return self.send(method='post', path=url, data=data)
+
+    def delete_saved_search(self, uuid):
+        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
+        url = self.get_common_saved_search_url(uuid)
+        return self.send(method='delete', path=url)
+
+    def get_saved_search_detail(self, uuid):
+        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
+        url = self.get_common_saved_search_url(uuid)
+        return self.send(method='get', path=url)
+
+    def get_type(self):
+        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
+        url = self.get_type_url()
+        return self.send(method='get', path=url)
+
+    def get_logsource(self):
+        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
+        url = self.get_logsource_url()
+        params = {"project_name": self.project_name, "page_size": 200, "page": 1}
+        return self.send(method='get', path=url, params=params)
+
+    def get_aggregations(self):
+        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
+        url = self.get_aggregations_url()
+        params = Common.generate_time_params()
+        return self.send(method='get', path=url, params=params)
+
+    def search_log(self, service_id):
+        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
+        url = self.get_search_url(service_id)
+        params = Common.generate_time_params()
+        params.update({"project_name": self.project_name, "size": 50, "pageno": 1})
+        return self.send(method='get', path=url, params=params)
+
+    def get_saved_search_uuid(self, name):
+        response = self.list_saved_search()
+        assert response.status_code == 200, '获取保存日志条件列表失败 {}'.format(response.text)
+        return self.get_uuid_accord_name(response.json(), {"name": name}, "uuid")
---
+++ test_case/log/test_log.py
@@ -0,0 +1,67 @@
+import pytest
+
+from test_case.application.app import Application
+from test_case.log.log import Log
+
+
+class TestLogSuite(object):
+    def setup_class(self):
+        self.log = Log()
+        self.log_name = 'alauda-log-{}'.format(self.log.region_name).replace('_', '-')
+        self.application = Application()
+        self.teardown_class(self)
+
+    def teardown_class(self):
+        uuid = self.log.get_saved_search_uuid(self.log_name)
+        self.log.delete_saved_search(uuid)
+
+    @pytest.mark.BAT
+    def test_log(self):
+        '''
+        获取日志源-获取查询类型-获取时间统计-日志查询
+        创建查询条件-获取条件详情-搜索查询条件-获取查询条件列表-删除查询条件
+        :return:
+        '''
+        result = {"flag": True}
+        # get logsource
+        logsource_result = self.log.get_logsource()
+        result = self.log.update_result(result, logsource_result.status_code == 200, '获取数据源失败')
+        result = self.log.update_result(result, 'default' in logsource_result.text, '获取数据源失败:default不在列表中')
+        # get types
+        types_result = self.log.get_type()
+        result = self.log.update_result(result, types_result.status_code == 200, '获取查询类型失败')
+        result = self.log.update_result(result, 'clusters' in types_result.json().keys(), '获取查询类型失败:clusters不在列表中')
+        # get aggregations
+        aggregation_result = self.log.get_aggregations()
+        result = self.log.update_result(result, aggregation_result.status_code == 200, '获取日志统计失败')
+        result = self.log.update_result(result, len(aggregation_result.json().get('buckets')) > 0, '获取日志统计失败:数据为空')
+        # search log
+        self.application.get_service_log(self.log.global_info['$GLOBAL_SERVICE_ID'], 'logglogloglog')
+        search_result = self.log.search_log(self.log.global_info['$GLOBAL_SERVICE_ID'])
+        result = self.log.update_result(result, search_result.status_code == 200, '按服务查询日志失败')
+        result = self.log.update_result(result, search_result.json().get('total_items') > 0, '按服务查询日志失败:日志为空')
+        # create saved_search
+        create_result = self.log.create_saved_search('./test_data/log/log.json', {'$log_name': self.log_name})
+        assert create_result.status_code == 200, "创建日志查询条件失败 {}".format(create_result.text)
+        log_id = self.log.get_value(create_result.json(), 'uuid')
+        # get saved_search detail
+        detail_result = self.log.get_saved_search_detail(log_id)
+        result = self.log.update_result(result, detail_result.status_code == 200, '获取日志查询条件详情失败')
+        result = self.log.update_result(result, len(detail_result.json().get('query_conditions')) == 3,
+                                        '获取日志查询条件详情失败:查询条件个数不是3')
+        # search  saved_search
+        search_saved = self.log.list_saved_search(self.log_name)
+        result = self.log.update_result(result, search_saved.status_code == 200,
+                                        '按名称查询日志查询条件失败 {}'.format(search_saved.text))
+        result = self.log.update_result(result, len(search_saved.json()) == 1, '按名称查询日志查询条件失败:结果为空')
+        # list saved_search
+        list_result = self.log.list_saved_search()
+        result = self.log.update_result(result, list_result.status_code == 200,
+                                        '获取日志查询条件列表失败 {}'.format(list_result.text))
+        result = self.log.update_result(result, self.log_name in list_result.text, '获取日志查询条件列表失败:新建日志查询条件不在列表中')
+        # delete saved_search
+        delete_result = self.log.delete_saved_search(log_id)
+        assert delete_result.status_code == 204, "删除日志查询条件失败 {}".format(delete_result.text)
+        delete_flag = self.log.check_exists(self.log.get_common_saved_search_url(log_id), 404)
+        assert delete_flag, "删除日志查询条件失败"
+        assert result['flag'], result
