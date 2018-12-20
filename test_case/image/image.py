fd95356e13d60045b5efd1637e613611ae3a1fe1
liuzongyao <liuzongyao@liuzongyaodeMacBook-Pro.local>
2018-12-20 16:42:44 +0800

    remove daemon and secret

--- test_case/image/image.py
+++ test_case/image/image.py
@@ -1,19 +1,10 @@
 from common.base_request import Common
 from common.log import logger
-import sys
-import json
-import time
-import paramiko
-import os
-import yaml
 
 
 class Image(Common):
     def __init__(self):
         super(Image, self).__init__()
-
-    def get_feature_url(self, region_name):
-        return "v2/regions/{}/{}/features".format(self.account, region_name)
 
     def create_repo_url(self, reg_project_name=None):
         if reg_project_name:
@@ -118,13 +109,13 @@ class Image(Common):
         path = self.get_delete_repo_tag_url(repo_name, tag_name, reg_project_name=reg_project_name)
         return self.send(method='delete', path=path, params={})
 
-    def get_repo_list(self, reg_project_name=None):
+    def get_repo_list(self, reg_project_name=None, params=None):
         path = self.get_repo_list_url(reg_project_name=reg_project_name)
-        return self.send(method='get', path=path, params={})
+        return self.send(method='get', path=path, params=params)
 
-    def get_reg_project(self, reg_project_name):
+    def get_reg_project(self, reg_project_name, params=None):
         path = self.get_reg_project_list_url()
-        response = self.send(method='get', path=path, params={})
+        response = self.send(method='get', path=path, params=params)
         if response.status_code == 200:
             contents = response.json()
             for content in contents:
@@ -141,166 +132,3 @@ class Image(Common):
     def get_image_detail(self, repo_name, repo_tag, reg_project=None):
         path = self.get_image_url(repo_name, repo_tag, reg_project=reg_project)
         return self.send(method='get', path=path, params={})
-
-    def get_feature_template(self, region_name, type):
-        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
-        url = self.get_feature_url(region_name)
-        response = self.send(method="get", path=url, params={})
-        if response.status_code != 200:
-            return {}
-        templates = self.get_value(response.json(), "{}.template.versions".format(type))
-        for template in templates:
-            if template["is_active"]:
-                content = template["values_yaml_content"]
-                return {
-                    "$values_yaml_content": yaml.load(content)
-                }
-        return {}
-
-    def install_registry(self, region_name, registry_name, file):
-        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
-        data = self.get_feature_template(region_name, "registry")
-        data['$values_yaml_content']['environment']['name'] = registry_name
-        data.update({"$values_yaml_content": yaml.dump(data["$values_yaml_content"])})
-        url = "v2/regions/{}/{}/features/registry".format(self.account, region_name)
-        data = self.generate_data(file, data).replace("\n", "\\n")
-        return self.send(method="post", path=url, data=data, params={})
-
-    def uninstall_registry(self, region_name):
-        logger.info(sys._getframe().f_code.co_name.center(50, '*'))
-        url = "v2/regions/{}/{}/features/registry".format(self.account, region_name)
-        return self.send(method="delete", path=url, params={})
-
-    def get_registry_running(self):
-        path = self.get_feature_url(self.region_name)
-        for i in range(1, 7):
-            result = self.send(method="get", path=path, params={})
-            status = result.json()['registry']['application_info']['status']
-            if status == 'Running':
-                break
-            else:
-                time.sleep(10)
-
-    def get_regin_id(self):
-        url = "v2/regions/{}/{}".format(self.account, self.region_name)
-        result = self.send(method="get", path=url, params={})
-        assert result.status_code == 200
-        return result.json()['id']
-
-    def get_registry_endpoint(self):
-        path = "v1/registries/{}".format(self.account)
-        result = self.send(method="get", path=path, params={})
-        assert result.status_code == 200
-        for i in range(len(result.json())):
-            if result.json()[i]['region_id'] == self.get_regin_id():
-                endpoint = result.json()[i]['endpoint']
-                return endpoint
-
-    def remote_exec_command(self, command, **kwargs):
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(**kwargs)
-        stdin, stdout, stderr = ssh.exec_command(command)
-        result = stdout.read()
-        ssh.close()
-        return result
-
-    def copy_from_local_to_jumper(self, file_name, host, port, username, path, password=None, key_filename=None):
-        if key_filename:
-            if port:
-                cmd = 'scp -i ' + key_filename + ' -P ' + str(
-                    port) + ' ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                os.system(cmd)
-            else:
-                cmd = 'scp -i ' + key_filename + ' ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                os.system(cmd)
-        else:
-            pass
-
-    def copy_from_jumper_to_local(self, file_name, host, port, username, path,  password=None, key_filename=None):
-        if key_filename:
-            if port:
-                cmd = 'scp -i ' + key_filename + ' -P ' + str(
-                    port) + ' ' + username + '@' + host + ':' + file_name + ' ' + path
-                logger.info(cmd)
-                os.system(cmd)
-            else:
-                cmd = 'scp -i ' + key_filename + ' ' + username + '@' + host + ':' + file_name + ' ' + path
-                logger.info(cmd)
-                os.system(cmd)
-        else:
-            pass
-
-    def copy_from_jumper_to_node(self, file_name, host, username, path, port=None, password=None, key_filename=None):
-        if key_filename:
-            if port:
-                cmd = 'scp -i ' + key_filename + ' -P ' + str(
-                    port) + ' ' + username + '@' + host + ':' + file_name + ' ' + path
-                logger.info(cmd)
-                return cmd
-            else:
-                cmd = 'scp -i ' + key_filename + ' ' + username + '@' + host + ':' + file_name + ' ' + path
-                logger.info(cmd)
-                return cmd
-        elif password:
-            if port:
-                cmd = 'sshpass -p ' + password + 'scp -P ' + str(port) + ' ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-            else:
-                cmd = 'sshpass -p ' + password + 'scp ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-        else:
-            if port:
-                cmd = 'scp -P ' + str(port) + ' ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-            else:
-                cmd = 'scp ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-
-    def copy_from_node_to_jumper(self, file_name, host, username, path, port=None, password=None, key_filename=None):
-        if key_filename:
-            if port:
-                cmd = 'scp -i ' + key_filename + ' -P ' + str(
-                    port) + ' ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-            else:
-                cmd = 'scp -i ' + key_filename + ' ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-        elif password:
-            if port:
-                cmd = 'sshpass -p ' + password + 'scp -P ' + str(
-                    port) + ' ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-            else:
-                cmd = 'sshpass -p ' + password + 'scp ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-        else:
-            if port:
-                cmd = 'scp -P ' + str(port) + ' ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-            else:
-                cmd = 'scp ' + file_name + ' ' + username + '@' + host + ':' + path
-                logger.info(cmd)
-                return cmd
-
-    def change_secrets(self, secret_name):
-        f = open('test_case/image/sa.json', encoding='utf-8')
-        res = json.load(f)
-        secrets = res['imagePullSecrets']
-        new_secret = {'name': secret_name}
-        secrets.append(new_secret)
-        f.close()
-        f1 = open('test_case/image/sa.json', mode='w', encoding='utf-8')
-        json.dump(res, f1)
-        f1.close()
--- test_case/image/test_images.py
+++ test_case/image/test_images.py
@@ -1,101 +1,30 @@
 from test_case.image.image import Image
 import pytest
-import os
-from common.log import logger
 
 
-@pytest.mark.BAT
+@pytest.mark.demo
 class TestImageSuite(object):
     def setup_class(self):
         self.image_tool = Image()
         self.index = 10
-        self.tencent = 'test_case/image/tencent.pem'
-        secret_name = 'pull-image-secret6'
-        self.add_daemon = 'test_case/image/add_daemon.py'
-        self.tag = 'helloworld'
-        # wait for houchao to add in the system setup
-        if self.image_tool.env != 'private':
-            self.image_tool.install_registry(self.image_tool.region_name, self.image_tool.registry_name,
-                                             "test_data/image/install_registry.json")
-            self.image_tool.get_registry_running()
-        self.registry_address = self.image_tool.get_registry_endpoint()
-        cmd = 'scp -i ' + self.tencent + ' -P 52022 ' + self.add_daemon + ' liuzongyao@118.24.213.153:/home/liuzongyao'
-        logger.info('copy file to tencent jump host ' + cmd)
-        os.system(cmd)
-
-        copy_add_daemon = 'scp add_daemon.py root@' + self.image_tool.command_ip + ':/root'
-
-        logger.info('copy file to the worker daemon host ' + copy_add_daemon)
-        self.image_tool.remote_exec_command(copy_add_daemon, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        copy_file = 'ssh -tt root@' + self.image_tool.command_ip + ' python add_daemon.py ' + self.registry_address
-        logger.info('copy file command ' + copy_file)
-        self.image_tool.remote_exec_command(copy_file, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        restart_docker = 'ssh -tt root@' + self.image_tool.command_ip + ' systemctl restart docker.service'
-        logger.info('restart docker command ' + restart_docker)
-        self.image_tool.remote_exec_command(restart_docker, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        docker_login = 'ssh -tt root@' + self.image_tool.command_ip + ' docker login -u ' + self.image_tool.account + \
-                       ' -p ' + self.image_tool.password + ' ' + self.registry_address
-        logger.info('docker login command ' + docker_login)
-        self.image_tool.remote_exec_command(docker_login, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        docker_pull = 'ssh -tt root@' + self.image_tool.command_ip + ' docker pull ' + self.image_tool.image
-        logger.info('docker login command ' + docker_pull)
-        self.image_tool.remote_exec_command(docker_pull, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        create_secrets = 'ssh -tt root@' + self.image_tool.k8s_ip + ' kubectl create secret docker-registry  ' \
-                         + secret_name + ' --docker-server=\'' + self.registry_address + '\' --docker-username=\'' \
-                         + self.image_tool.account + '\' --docker-password=\'' + self.image_tool.password \
-                         + '\' ' '--docker-email=highcareer@126.com -n ' + self.image_tool.k8s_namespace
-        logger.info('create secrets command ' + create_secrets)
-
-        self.image_tool.remote_exec_command(create_secrets, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        # sa.json from k8s to tencent to local
-
-        get_sa = 'ssh -tt root@' + self.image_tool.k8s_ip + ' kubectl get sa default -o json -n ' \
-                 + self.image_tool.k8s_namespace + ' > sa.json'
-        logger.info('create secrets command ' + get_sa)
-
-        self.image_tool.remote_exec_command(get_sa, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        self.image_tool.copy_from_jumper_to_local('/home/liuzongyao/sa.json', '118.24.213.153', 52022, 'liuzongyao',
-                                                  'test_case/image', key_filename=self.tencent)
-
-        self.image_tool.change_secrets(secret_name)
-
-        self.image_tool.copy_from_local_to_jumper('test_case/image/sa.json', '118.24.213.153', 52022, 'liuzongyao',
-                                                  '/home/liuzongyao', key_filename=self.tencent)
-
-        cmd = self.image_tool.copy_from_jumper_to_node('sa.json', '62.234.114.88', 'root', '/root')
-
-        self.image_tool.remote_exec_command(cmd, hostname='118.24.213.153', port=52022, username='liuzongyao',
-                                            key_filename=self.tencent)
-
-        replace_sa = 'ssh -tt root@' + self.image_tool.k8s_ip + ' kubectl replace sa default -f sa.json ' \
-                     + self.image_tool.k8s_namespace
-        logger.info('replace sa pull image secrets command ' + replace_sa)
-
-        self.image_tool.remote_exec_command(replace_sa, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
         self.update_repo = "update daemon"
         self.reg_project_name = self.image_tool.region_name
         self.repo_name = self.image_tool.region_name
 
     def teardown_class(self):
-        pass
-        # self.image_tool.uninstall_registry(self.image_tool.region_name)
-        # jenkins还需要镜像仓库 不应该在此次删除 应该在系统teardown里删除
+        self.image_tool.delete_reg_project(self.reg_project_name + 'params')
+
+    @pytest.mark.skip
+    def test_repo_project_params(self):
+        create_reg_ret = self.image_tool.create_reg_project('./test_data/image/create_reg_project.yaml',
+                                                            {"$REG_PROJECT_NAME": self.reg_project_name + 'params'})
+        assert create_reg_ret.status_code == 201, "创建镜像项目操作失败"
+        get_reg_project = self.image_tool.get_reg_project(self.reg_project_name)
+        assert get_reg_project, "获得镜像项目失败"
+        get_reg_project = self.image_tool.get_reg_project(self.reg_project_name, self.image_tool.params)
+        assert get_reg_project, "获得项目空间下镜像项目失败"
+        delete_reg_ret = self.image_tool.delete_reg_project(self.reg_project_name)
+        assert delete_reg_ret.status_code == 204, "删除镜像项目操作失败"
 
     def test_multiple_project(self):
         # create registry project
@@ -211,74 +140,4 @@ class TestImageSuite(object):
 
         # delete repo
         delete_repo_ret = self.image_tool.delete_repo(self.repo_name)
-        assert delete_repo_ret.status_code == 204, "删除镜像仓库操作失败"
-
-    def test_project_push_image(self):
-        create_reg_ret = self.image_tool.create_reg_project('./test_data/image/create_reg_project.yaml',
-                                                            {"$REG_PROJECT_NAME": self.reg_project_name})
-        assert create_reg_ret.status_code == 201, "创建镜像项目操作失败"
-
-        # create repo
-        create_repo_ret = self.image_tool.create_repo('./test_data/image/create_repo.yaml',
-                                                      {"$REPO_IMAGE": self.repo_name},
-                                                      reg_project_name=self.reg_project_name)
-        assert create_repo_ret.status_code == 201, "创建镜像仓库操作失败"
-
-        docker_tag = 'ssh -tt root@' + self.image_tool.command_ip + ' docker tag ' + self.image_tool.image + ' ' \
-                     + self.registry_address + '/'+self.repo_name+'/'+self.reg_project_name+':'+self.tag
-        logger.info('docker login command ' + docker_tag)
-        self.image_tool.remote_exec_command(docker_tag, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        docker_push = 'ssh -tt root@' + self.image_tool.command_ip + ' docker push ' + self.registry_address +\
-                      '/'+self.repo_name+'/'+self.reg_project_name+':'+self.tag
-        logger.info('docker login command ' + docker_push)
-        self.image_tool.remote_exec_command(docker_push, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        get_repo_tag = self.image_tool.get_repo_tag(self.repo_name, self.reg_project_name)
-        assert get_repo_tag.status_code == 200, "创建镜像仓库操作失败"
-
-        get_image_detail = self.image_tool.get_image_detail(self.repo_name, self.tag, self.reg_project_name)
-        assert get_image_detail.status_code == 200, "创建镜像仓详情失败"
-
-        get_repo_detail = self.image_tool.get_repo_detail(self.repo_name, self.reg_project_name)
-
-        assert get_repo_detail.json()['upload'] >= 1
-
-        delete_repo_ret = self.image_tool.delete_repo(self.repo_name, self.reg_project_name)
-        assert delete_repo_ret.status_code == 204, "删除镜像仓库操作失败"
-
-        delete_reg_ret = self.image_tool.delete_reg_project(self.reg_project_name)
-        assert delete_reg_ret.status_code == 204, "删除镜像项目操作失败"
-
-    def test_shared_push_image(self):
-        create_repo_ret = self.image_tool.create_repo('./test_data/image/create_repo.yaml',
-                                                      {"$REPO_IMAGE": self.repo_name})
-        assert create_repo_ret.status_code == 201, "创建镜像仓库操作失败"
-
-        docker_tag = 'ssh -tt root@' + self.image_tool.command_ip + ' docker tag ' + self.image_tool.image + ' ' +\
-                     self.registry_address + '/' + self.repo_name + ':' + self.tag
-        logger.info('docker login command ' + docker_tag)
-        self.image_tool.remote_exec_command(docker_tag, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        docker_push = 'ssh -tt root@' + self.image_tool.command_ip + ' docker push ' + self.registry_address + '/' + \
-                      self.repo_name + ':' + self.tag
-        logger.info('docker login command ' + docker_push)
-        self.image_tool.remote_exec_command(docker_push, hostname='118.24.213.153', port=52022,
-                                            username='liuzongyao', key_filename=self.tencent)
-
-        get_repo_tag = self.image_tool.get_repo_tag(self.repo_name)
-        assert get_repo_tag.status_code == 200, "创建镜像仓库操作失败"
-
-        get_image_detail = self.image_tool.get_image_detail(self.repo_name, self.tag)
-        assert get_image_detail.status_code == 200, "创建镜像仓详情失败"
-
-        get_repo_detail = self.image_tool.get_repo_detail(self.repo_name)
-
-        assert get_repo_detail.json()['upload'] >= 1
-
-        delete_repo_ret = self.image_tool.delete_repo(self.repo_name)
-
         assert delete_repo_ret.status_code == 204, "删除镜像仓库操作失败"
